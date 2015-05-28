a9bee7e7598d5f99f00a18ce1bc49b5a27aa5d4a
Robin Dickson <robindickson@gmail.com>
2015-05-28 17:26:05 +0200

    tidying

--- rock_scissors_paper.rb
+++ rock_scissors_paper.rb
@@ -1,7 +1,9 @@
 # rock_scissors_paper.rb
 
 CHOICES = { 'r' => 'rock', 's' => 'scissors', 'p' => 'paper' }
-SYMBOLS = { 'r' => "\xe2\x9c\x8a", 's' => "\xe2\x9c\x8c", 'p' => "\xe2\x9c\x8b"}
+SYMBOLS = { 'r' => "\xe2\x9c\x8a",
+            's' => "\xe2\x9c\x8c",
+            'p' => "\xe2\x9c\x8b" }
 
 def say_then_pause(message)
   puts message
@@ -12,20 +14,20 @@ loop do
   opponent = "#{CHOICES.values.sample.capitalize} Head"
 
   puts "I am #{opponent}, henchman of the King!"
-  say_then_pause "I challenge you to a Janken match!"
-  puts "You must choose either rock (r), scissors (s) or paper (p)"
+  say_then_pause 'I challenge you to a Janken match!'
+  puts 'You must choose either rock (r), scissors (s) or paper (p)'
 
   player_choice = gets.chomp.downcase
 
   until CHOICES.include?(player_choice)
-    puts "Error - you must choose rock (r), scissors (s) or paper (p)"
+    puts 'Error - you must choose rock (r), scissors (s) or paper (p)'
     player_choice = gets.chomp
   end
 
   computer_choice = CHOICES.keys.sample
   sleep 2
 
-  say_then_pause "Saisho wa gu, janken pon!"
+  say_then_pause 'Saisho wa gu, janken pon!'
   say_then_pause "You throw #{CHOICES[player_choice]}  " \
   "#{SYMBOLS[player_choice]}"
   say_then_pause "#{opponent} throws #{CHOICES[computer_choice]}  " \
@@ -37,11 +39,11 @@ loop do
   elsif (player_choice == 'r' && computer_choice == 's') ||
         (player_choice == 's' && computer_choice == 'p') ||
         (player_choice == 'p' && computer_choice == 'r')
-        say_then_pause "You win!"
+    say_then_pause 'You win!'
   else
     say_then_pause "#{opponent} wins!"
   end
 
-  puts "Try again? (y/n)"
+  puts 'Try again? (y/n)'
   break if gets.chomp.downcase != 'y'
 end
